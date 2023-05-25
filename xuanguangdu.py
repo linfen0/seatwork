1a01f45bbba31a2588d5d2297f273897665de07a
rinflan <92668709+linfen0@users.noreply.github.com>
2023-05-25 23:33:48 +0800

    Update xuanguangdu.py

--- xuanguangdu.py
+++ xuanguangdu.py
@@ -1,6 +1,6 @@
 from base import physical_base
 import numpy as np
-# 在这个alpha数组里面，填你自己测量的旋光度的数据，之后运行即可
+# 在这个名为alpha的list里面，填你自己测量的旋光度的数据，之后运行即可
 alpha=[8.15,8.40,8.55,8.30,8.50]
 alpha=np.array([alpha],dtype=np.float32)
 newproject=physical_base.physicDataProcess(alpha)
